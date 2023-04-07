81fe79d32891cb7a85ed3829a6e726460f9b6e42
hyunggilwoo <hyunggilswoo@gmail.com>
2023-04-06 22:51:18 -0700

    Initial query commands are created here.

---
+++ qry_commands.sql
@@ -0,0 +1,6 @@
+-- For now, we will have us execute this command to create the table.
+CREATE TABLE Customers (Customer_ID int PRIMARY KEY, Name VARCHAR(256), 
+                        Address VARCHAR(1024), phone_number Varchar(256) , email VARCHAR(256));
+INSERT INTO Customers (Customer_ID, Name, Address, phone_number, email) VALUES 
+    (00123, "Rowan Osmon", "278 Bleecker Street, New York City, NY 10014", "212-243-1680")
+-- Customers (Name, address, phone, email, authorize contacts, vehicles, customer number)
