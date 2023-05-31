27fe57e93c53578fe81a761b0142184e169291fc
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2023-05-31 14:16:24 -0700

    Working on intake form

--- src/server/mySQL.js
+++ src/server/mySQL.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const app = express();
-const port = 3001; // Choose a suitable port number
+const port = 3006; // Choose a suitable port number
 
 
 app.use(cors());
